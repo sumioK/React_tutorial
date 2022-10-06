91e8db096682c04625e5d55c0ead7b5c896cd0bf
Sumio K <tap.miwo@gmail.com>
2022-10-07 06:00:50 +0900

    add input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import TodoList from "./TodoList"
 
 function App() {
-  return <div>
-
-    
+  return( 
+  <div>
+    <TodoList />
+    <input type="text" />
+    <button>タスクを追加</button>
+    <button>完了したタスクの削除</button>
+    <div>残りのタスク:0</div>
   </div>
+  );
 }
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const TodoList = () => {
+    return (
+        <div>TodoList</div>
+    )
+}
+export default TodoList
