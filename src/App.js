e478a16ab7f4cbeebf482513c8d4ed4ccb08266f
Sumio K <tap.miwo@gmail.com>
2022-10-08 18:16:44 +0900

    clear method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useRef} from "react";
 import TodoList from "./TodoList"
 import {v4 as uuidv4 } from 'uuid'
+import todo from "./Todo";
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: "Todo1", completed: false},]);
+  const [todos, setTodos] = useState([
+  ]);
 
   const todoNameRef = useRef();
 
@@ -16,18 +18,23 @@ function App() {
 
   const toggleTodo = (id) =>{
     const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id == id);
+    const todo = newTodos.find((todo) => todo.id === id);
     todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  const handleClear = () =>{
+    const newTodos = todos.filter((todo) => !todo.completed);
     setTodos(newTodos);
   }
 
   return( 
   <div>
-    <TodoList todos={todos} toggleTodo={toggleTodo}/>
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
     <input type="text" ref={todoNameRef}/>
     <button onClick={handleAddTodo}>タスクを追加</button>
-    <button>完了したタスクの削除</button>
-    <div>残りのタスク:0</div>
+    <button onClick={handleClear}>完了したタスクの削除</button>
+    <div>残りのタスク:{todos.filter(() => !todo.completed).length}</div>
   </div>
   );
 }
